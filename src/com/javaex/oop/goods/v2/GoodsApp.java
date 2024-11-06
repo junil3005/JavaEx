431eb95a070dbf67d8b3388e4e29099693b01c25
junil3005 <yjl3005@gamil.com>
2024-11-06 16:53:12 +0900

    필드와 접근자 메서드 작성

---
+++ src/com/javaex/oop/goods/v2/GoodsApp.java
@@ -0,0 +1,39 @@
+package com.javaex.oop.goods.v2;
+
+// Goods v1. 필드
+class Goods {  // 디폴트
+	// 필드 선언 -> private
+	 String name;  // private
+	 int price;	  // private
+}
+// Goods 테스트 클래스
+	public class GoodsApp { // 퍼블릭
+
+	public static void main(String[] args) {
+		Goods camera = new Goods();
+		// 생성자를 호출 -> 인스턴스 생성 -> camera로 참조
+		
+		camera.name = "Nikon";
+		camera.price = 400_000;
+		System.out.printf("%s -> %,d%n", camera.name,
+				camera.price);
+		
+		Goods notebook = new Goods();
+		notebook.name = "LG그램";
+		notebook.price = 900_0000;
+		
+		System.out.printf("%s -> %,d%n", notebook.name,
+					notebook.price);
+		
+		Goods mug = new Goods();
+		mug.name = "머그컵";
+		mug.price = 2_000;
+		
+		System.out.printf("%s - > %,d%n", mug.name,
+				mug.price);
+
+	}
+
+	 		
+}		
+	
---
+++ src/java/practice/GoodApp.java
@@ -0,0 +1,10 @@
+package java.practice;
+
+public class GoodApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
