701906e614f19720b1e78afe7378f95af3edad76
junil3005 <yjl3005@gamil.com>
2024-11-06 10:53:26 +0900

    main 메서드의 매개변수

---
+++ src/com/javaex/basic/reftypes/MainArgs.java
@@ -0,0 +1,14 @@
+package com.javaex.basic.reftypes;
+
+import java.util.Arrays;
+
+public class MainArgs {
+
+	public static void main(String[] args) {
+		// 매개변수 목록이 args로 전달된다 (너의 정체가뭐냐?)
+		System.out.println("전달된 매게 변수:" +
+				Arrays.toString(args));
+
+	}
+
+}
