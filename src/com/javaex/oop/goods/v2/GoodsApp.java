88ae02fc469e934b4359d019289ef25b7a4131fb
junil3005 <yjl3005@gamil.com>
2024-11-07 11:10:41 +0900

    Getters and Setters

--- src/com/javaex/oop/goods/v2/GoodsApp.java
+++ src/com/javaex/oop/goods/v2/GoodsApp.java
@@ -3,8 +3,32 @@ package com.javaex.oop.goods.v2;
 // Goods v1. 필드
 class Goods {  // 디폴트생략
 	// 필드 선언 -> private
-	 String name;  // private
-	 int price;	  // private
+	// 정보는 은닉하고 Getter와 Setter를 통한 우회 접근이 필요
+	private String name;  // private
+	private int price;	  // private
+	
+	// Getters / Setters
+	public String getName() {
+		return name; // 네임필드를 위한 게터
+		
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+		// this -> 현재 인스턴스 자체		
+	}
+	
+	public int getPrice() { // 파라미터 x
+		return price;
+		
+	}
+	
+	public void setPrice(int price) {
+		this.price = price;
+		
+	}
+	
+	
 }
 // Goods 테스트 클래스
 	public class GoodsApp { // 퍼블릭
@@ -13,24 +37,31 @@ class Goods {  // 디폴트생략
 		Goods camera = new Goods();
 		// 생성자를 호출 -> 인스턴스 생성 -> camera로 참조
 		
-		camera.name = "Nikon";
-		camera.price = 400_000;
-		System.out.printf("%s -> %,d%n", camera.name,
-				camera.price);
+//		camera.name = "Nikon";
+//		camera.price = 400_000;
+		camera.setName("Nikon");
+		camera.setPrice(400_000);
+		
+//		System.out.printf("%s -> %,d%n", camera.name,
+////				camera.price);
+//		
+		System.out.printf("%s -> %,d%n", camera.getName(),
+				camera.getPrice());
+		
 		
 		Goods notebook = new Goods();
-		notebook.name = "LG그램";
-		notebook.price = 900_0000;
+		notebook.setName("LG그램");
+		notebook.setPrice(900_0000);
 		
-		System.out.printf("%s -> %,d%n", notebook.name,
-					notebook.price);
+		System.out.printf("%s -> %,d%n", notebook.getName(),
+					notebook.getPrice());
 		
 		Goods mug = new Goods();
-		mug.name = "머그컵";
-		mug.price = 2_000;
+		mug.setName("머그컵");
+		mug.setPrice(2_000);
 		
-		System.out.printf("%s - > %,d%n", mug.name,
-				mug.price);
+		System.out.printf("%s - > %,d%n", mug.getName(),
+				mug.getPrice());
 
 	}
 
