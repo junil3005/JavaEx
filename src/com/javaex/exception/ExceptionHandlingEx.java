c21018ae43803c6465960a172ffbe216f55afee7
junil3005 <yjl3005@gamil.com>
2024-11-11 10:54:12 +0900

    null 포인터 익셉션, Array 익셉션

--- src/com/javaex/exception/ExceptionHandlingEx.java
+++ src/com/javaex/exception/ExceptionHandlingEx.java
@@ -5,8 +5,31 @@ import java.util.Scanner;
 public class ExceptionHandlingEx {
 
 	public static void main(String[] args) {
-		arithExceptionEx();
+//		arithExceptionEx();
+		arrayExceptionEx();
+		nullPointerException();
 
+	}
+	
+	private static arrayExceptionEx() {
+		int[] intarray = new int[] { 3, 6, 9 };
+		
+		try {
+			System.out.println(intArray[3]);
+		}	catch (ArrayIndexOutOfBoundsException e) {
+			System.err.println("Error:" + e.getMessage());
+		}
+	
+	}
+	
+	private static void nullPointerExceptionEx() {
+		String str = new String("Hello Java");
+		try {
+		str = null;
+		System.out.println(str.toUpperCase());
+		} catch (NullPointerException e) {
+			System.err.println("널입니다.");
+		}
 	}
 	
 	private static void arithExceptionEx() {
@@ -26,15 +49,18 @@ public class ExceptionHandlingEx {
 		} catch (InputMismatchException e) {
 		System.err.println("정수를 입력해 주세요");
 		} catch (ArithmeticException e) {
-			System.err.println("0으로는 나눌 수 없습니다.");
+		System.err.println("0으로는 나눌 수 없습니다.");
 		} catch (Exception e) {
 			//	가장 마지막에 남아 있는 예외 처리를 위해
 			e.printStackTrace();
 		} finally {
 			//	예외 유무 관계 없이 가장 마지막에 실행
-			// 자원 정리 등을 수행
+			//  자원 정리 등을 수행
 			System.out.println("예외 처리 종료!");
 		}
+		
+		scanner.close();
+		System.out.println("프로그램 종료!");
 	
 	}
 
